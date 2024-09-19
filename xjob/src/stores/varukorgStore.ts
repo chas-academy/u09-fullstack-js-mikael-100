7a5ef4021c326d9ff92c766eef8b746f1dfd8f34
Mikael Jakobsson <Maadmike@hotmail.se>
2024-09-19 21:24:19 +0200

    Gått igenom Zustans store

--- xjob/src/stores/varukorgStore.ts
+++ xjob/src/stores/varukorgStore.ts
@@ -12,6 +12,7 @@ export const useVarukorgStore = create<VarukorgState>((set) => ({
     set((state) => ({
       items: {
         ...state.items,
+        // Den första [idDish]: är de inkommande värdet som med (state.items[idDish] || 0) kollar om det finns något matchande nyckel i objectet. Och det finns plussas de på med antal. OBS (det kan bara finnas unika nycklar i ett object så det kan ej finnas två identiska, en skulle i sådana fall överskrivas)
         [idDish]: (state.items[idDish] || 0) + antal, // Addera det nya antalet till det befintliga
       },
     })),
@@ -19,6 +20,7 @@ export const useVarukorgStore = create<VarukorgState>((set) => ({
     set((state) => {
       const newItems = { ...state.items }; // Skapa en kopia av det nuvarande state.items
       delete newItems[idDish]; // Ta bort objektet med nyckeln idDish
+      // return { items: newItems } menas att man returnerar ett nytt object och i detta fall kopian av newItems där idDish nyckelvärdepar är bortaget.
       return { items: newItems }; // Returnera det uppdaterade state
     }),
 }));
